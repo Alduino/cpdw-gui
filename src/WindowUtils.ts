c5e74d150aa654b2c5da5e22bdbb677e904a1427
Zach Barham <git@alduino.dev>
2021-03-07 16:09:59 +1000

    Render inside requestAnimationFrame
    Lets Spector work properly

--- src/WindowUtils.ts
+++ src/WindowUtils.ts
@@ -4,8 +4,13 @@ function hrTimeToMs(hrtime: [number, number] = process.hrtime()) {
     return hrtime[0] * 1000 + hrtime[1] / 1000000;
 }
 
-function nextFrame() {
-    return new Promise(yay => requestAnimationFrame(yay));
+function nextFrame(cb: Function) {
+    return new Promise<void>(yay => {
+        requestAnimationFrame(() => {
+            cb();
+            yay();
+        });
+    });
 }
 
 export async function perFrame(win: WindowWrapper, fn: (delta: number, time: number) => void) {
@@ -14,16 +19,16 @@ export async function perFrame(win: WindowWrapper, fn: (delta: number, time: num
     let lastT = msFunction(), enabled = true;
 
     while (enabled && !win.closing) {
-        await nextFrame();
+        await nextFrame(() => {
+            const t = msFunction();
+            lastT = t;
 
-        const t = msFunction();
-        lastT = t;
-
-        try {
-            fn(t - lastT, t);
-        } catch (e) {
-            console.error(e);
-            enabled = false;
-        }
+            try {
+                fn(t - lastT, t);
+            } catch (e) {
+                console.error(e);
+                enabled = false;
+            }
+        });
     }
 }
